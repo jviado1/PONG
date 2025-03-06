bfc4d781412e8c6af0269da5f5c337e2bcc625cb
jviado1 <jviado@asu.edu>
2025-03-06 13:00:34 -0500

    add middle net, optimize paddle class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from paddle import Paddle
 
 screen = Screen()
@@ -6,15 +6,36 @@ screen.setup(800,600)
 screen.bgcolor("black")
 screen.title("PONG")
 screen.listen()
+screen.tracer(0)
 
-left_paddle = Paddle()
-right_paddle = Paddle()
-left_paddle.goto(-350,0)
-right_paddle.goto(350,0)
+net = Turtle()
+net.color("white")
+net.pu()
+net.hideturtle()
+net.setheading(90)
+net.goto(x=0,y=-300)
+net.pencolor("white")
+for _ in range(0,18):
+    net.pd()
+    net.forward(20)
+    net.pu()
+    net.forward(20)
+
+
+left_paddle = Paddle(-350,0)
+right_paddle = Paddle(350,0)
+
+print(left_paddle.position())
+print(right_paddle.position())
 
 screen.onkey(left_paddle.move_up,"w")
 screen.onkey(left_paddle.move_down, "s")
 screen.onkey(right_paddle.move_up,"Up")
 screen.onkey(right_paddle.move_down, "Down")
+
+game_on = True
+while game_on:
+    screen.update()
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,8 +1,9 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self,x_position,y_position):
         super().__init__()
+        self.goto(x_position,y_position)
         self.shape("square")
         self.shapesize(1,6)
         self.setheading(90)
@@ -10,7 +11,7 @@ class Paddle(Turtle):
         self.pu()
 
     def move_up(self):
-        self.forward(20)
+        self.forward(40)
 
     def move_down(self):
-        self.back(20)
+        self.back(40)
